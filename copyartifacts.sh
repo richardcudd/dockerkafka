726217b7b7610bffe45da5aa1332f6e8515631a1
Rich Cudd <richard.cudd@gmail.com>
2022-08-09 19:58:00 +0100

    more script files and change of location

--- copyartifacts.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Copy the properties file
-scp -i "~/.ssh/rcudd-ireland-key.pem" ~/dockerkafka/getting-started.properties ubuntu@ec2-3-249-55-33.eu-west-1.compute.amazonaws.com:/home/ubuntu/confluent
-
-# Copy the jar file
-scp -i "~/.ssh/rcudd-ireland-key.pem" ~/dockerkafka/out/artifacts/dockerkafka_jar2/dockerkafka.jar ubuntu@ec2-3-249-55-33.eu-west-1.compute.amazonaws.com:/home/ubuntu/confluent
