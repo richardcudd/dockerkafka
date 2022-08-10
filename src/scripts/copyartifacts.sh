3c5ffc5cbb435d725f26b026294fae9d4595646b
Rich Cudd <richard.cudd@gmail.com>
2022-08-10 19:10:42 +0100

    copy the files and run a remote jar to attach to

--- src/scripts/copyartifacts.sh
+++ src/scripts/copyartifacts.sh
@@ -11,10 +11,13 @@ chmod u+x ~/dockerkafka/src/scripts/producer.sh
 scp -i "~/.ssh/rcudd-ireland-key.pem" ~/dockerkafka/getting-started.properties ${SERVER_PATH}:${DEPLOYMENT_FOLDER}
 
 # Copy the jar file
-scp -i "~/.ssh/rcudd-ireland-key.pem" ~/dockerkafka/out/artifacts/dockerkafka_jar2/dockerkafka.jar ubuntu@ec2-3-249-55-33.eu-west-1.compute.amazonaws.com:/home/ubuntu
+scp -i "~/.ssh/rcudd-ireland-key.pem" ~/dockerkafka/out/artifacts/dockerkafka_jar2/dockerkafka.jar ${SERVER_PATH}:${DEPLOYMENT_FOLDER}
+
+# Run the jar file ready for a debug
+ssh -i "~/.ssh/rcudd-ireland-key.pem" ${SERVER_PATH} "java -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=*:5005 -cp ~/confluent/dockerkafka.jar examples.ProducerExample ~/confluent/getting-started.properties > yourservice.log 2>&1 &"
 
 # Copy the run script & move script
-scp -i "~/.ssh/rcudd-ireland-key.pem" ~/dockerkafka/src/scripts/deployfiles.sh ubuntu@ec2-3-249-55-33.eu-west-1.compute.amazonaws.com:/home/ubuntu
-scp -i "~/.ssh/rcudd-ireland-key.pem" ~/dockerkafka/src/scripts/consumer.sh ubuntu@ec2-3-249-55-33.eu-west-1.compute.amazonaws.com:/home/ubuntu
-scp -i "~/.ssh/rcudd-ireland-key.pem" ~/dockerkafka/src/scripts/producer.sh ubuntu@ec2-3-249-55-33.eu-west-1.compute.amazonaws.com:/home/ubuntu
+#scp -i "~/.ssh/rcudd-ireland-key.pem" ~/dockerkafka/src/scripts/deployfiles.sh ubuntu@ec2-3-249-55-33.eu-west-1.compute.amazonaws.com:/home/ubuntu
+#scp -i "~/.ssh/rcudd-ireland-key.pem" ~/dockerkafka/src/scripts/consumer.sh ubuntu@ec2-3-249-55-33.eu-west-1.compute.amazonaws.com:/home/ubuntu
+#scp -i "~/.ssh/rcudd-ireland-key.pem" ~/dockerkafka/src/scripts/producer.sh ubuntu@ec2-3-249-55-33.eu-west-1.compute.amazonaws.com:/home/ubuntu
 
